a83b48e66fab81ea7da118bc3c4d93753703f3b7
Beck Wangthumboon <becktsm@gmail.com>
2025-11-19 13:33:00 -0600

    added clerk webhooks

--- convex/users.ts
+++ convex/users.ts
@@ -1,4 +1,10 @@
-import type { MutationCtx, QueryCtx } from "./_generated/server";
+import {
+  internalMutation,
+  type MutationCtx,
+  type QueryCtx,
+} from "./_generated/server";
+import { v, type Validator } from "convex/values";
+import { UserJSON } from "@clerk/backend";
 
 export async function getUserByExternalId(
   ctx: QueryCtx | MutationCtx,
@@ -9,3 +15,29 @@ export async function getUserByExternalId(
     .withIndex("by_externalId", (q) => q.eq("externalId", externalId))
     .unique();
 }
+
+export const upsertFromClerk = internalMutation({
+  args: { data: v.any() as Validator<UserJSON> },
+  handler: async (ctx, { data }) => {
+    const existing = await getUserByExternalId(ctx, data.id);
+    if (!existing) {
+      await ctx.db.insert("users", {
+        externalId: data.id,
+      });
+    }
+  },
+});
+
+export const deleteFromClerk = internalMutation({
+  args: { clerkUserId: v.string() },
+  handler: async (ctx, { clerkUserId }) => {
+    const user = await getUserByExternalId(ctx, clerkUserId);
+    if (user) {
+      await ctx.db.delete(user._id);
+    } else {
+      console.warn(
+        `Can't delete user, there is none for Clerk user ID: ${clerkUserId}`,
+      );
+    }
+  },
+});
